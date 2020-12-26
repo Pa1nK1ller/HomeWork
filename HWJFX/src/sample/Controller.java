6c63c9a6db695f897b78603ac1f0aeaffeb33c9d
Pa1nK1ller <Kostya.300397@gmail.com>
2020-12-26 12:42:31 +0300

    8е дз

--- HWJFX/src/sample/Controller.java
+++ HWJFX/src/sample/Controller.java
@@ -6,33 +6,72 @@ import javafx.scene.control.*;
 import java.util.Random;
 
 public class Controller {
-
+    int count = 3;
+    int guess = -1;
+    int number;
     @FXML
-    private Label randomNum;
-
+    private Label message;
     @FXML
-    private TextField numberInput;
+    private TextField checkNumber;
 
     public void initialize() {
+        Random rnd = new Random();
+        number = rnd.nextInt(10);
+        restartButton.setVisible(false);
+        message.setVisible(true);
+        checkNumber.setVisible(true);
+        button.setVisible(true);
+        count = 3;
+        message.setText("Я Загадал число от 1 до 9 попробуй угадать");
+        exit.setVisible(false);
 
     }
 
     @FXML
-    void doRandomAction() {
-        Random random = new Random();
-
-        String message = numberInput.getText();
-        numberInput.clear();
-        if (!message.isBlank()) {
-            try {
-                int num = Integer.parseInt(message);
-                int randomNumber = random.nextInt(num + 1);
-                randomNum.setText(String.valueOf(randomNumber));
-            } catch (NumberFormatException e) {
-                e.printStackTrace();
-                new Alert(Alert.AlertType.ERROR,"Введите число").show();
-            }
+    void checkAnswer() {
+        try {
+            guess = Integer.parseInt(checkNumber.getText());
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            var alert = new Alert(Alert.AlertType.ERROR, "Ошибка ввода данных\nВ следующий раз думай, что вводишь! :)");
+            alert.setTitle("Ошибка ввода данных");
+            alert.show();
+        }
+        checkNumber.clear();
+        if (guess == number) {
+            message.setText("Вы Угадали");
+            restartButton.setVisible(true);
+            checkNumber.setVisible(false);
+            button.setVisible(false);
+            exit.setVisible(true);
+        } else {
+            count--;
+            message.setText(guess + ((guess > number) ? " Больше" : " Меньше") + " загаданной цифры\n" +
+                    "Количество оставшихся попыток:" + count);
+        }
+        if (count == 0) {
+            message.setText("Вы проиграли");
+            restartButton.setVisible(true);
+            checkNumber.setVisible(false);
+            button.setVisible(false);
+            exit.setVisible(true);
         }
     }
 
+    @FXML
+    private Button restartButton;
+    @FXML
+    private Button button;
+    @FXML
+    private Button exit;
+
+    @FXML
+    void restartGame() {
+        initialize();
+    }
+
+    @FXML
+    void exit() {
+        System.exit(-1);
+    }
 }
--- HWJFX/src/sample/Main.java
+++ HWJFX/src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Угадай цифру");
+        primaryStage.setScene(new Scene(root, 350, 300));
         primaryStage.show();
     }
 
